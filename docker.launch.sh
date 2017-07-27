93273df82c42eaf75858f34cf6b663a6bf54b910
agate <agate.hao@gmail.com>
2017-07-28 07:47:11 +0800

    clone instead of build entire app into docker

--- docker.launch.sh
+++ docker.launch.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+set -xe
+
 export NVM_DIR=/usr/local/nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
+MAD_DIR=${MAD_DIR:-/opt/mad}
+REPO=https://github.com/agate/mad.git
+
+if [ ! -d $MAD_DIR ]; then
+  CLONE_CMD="git clone $REPO --depth 1"
+  [ $BRANCH ] && CLONE_CMD="${CLONE_CMD} --branch ${BRANCH}"
+  $CLONE_CMD $MAD_DIR
+fi
+
 cd $MAD_DIR
 
+if [ ! -d node_modules ]; then
+  cp -r $TMP_MAD_PATH/node_modules .
+fi
+
 nvm use
+npm install
 node index.js
